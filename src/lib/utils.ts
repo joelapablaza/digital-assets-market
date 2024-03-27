4d22daa636c12d2ddaf2d14fd6455310d156ab7f
joelapablaza <joelapablaza@gmail.com>
2024-03-27 18:59:14 -0300

    fat: added currency format utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(
+  price: number | string,
+  options?: {
+    currency?: 'USD' | 'EUR' | 'ARS';
+    notation?: Intl.NumberFormatOptions['notation'];
+  }
+) {
+  const { currency, notation } = options
+    ? options
+    : {
+        currency: 'ARS',
+        notation: 'compact' as Intl.NumberFormatOptions['notation'],
+      };
+
+  const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat('es-ES', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
