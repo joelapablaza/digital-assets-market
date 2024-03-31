e06f50c1f1a6953a94ab56e5dcc9befc96195fa9
joelapablaza <joelapablaza@gmail.com>
2024-03-31 20:30:45 -0300

    feat added component to show products on main page, added carousel img, and the logic to get the public data from payload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import BecomeSeller from '@/components/become-seller';
 import Header from '@/components/header';
 import MaxWithWrapper from '@/components/max-with-wrapper';
 import Perks from '@/components/perks';
+import ProductReel from '@/components/product-reel';
 
 export default function Home() {
   return (
@@ -11,6 +12,11 @@ export default function Home() {
       </MaxWithWrapper>
       <MaxWithWrapper className="max-w-screen-lg">
         <BecomeSeller />
+        <ProductReel
+          query={{ sort: 'desc', limit: 8 }}
+          href="/products?sort=recent"
+          title="Novedades"
+        />
       </MaxWithWrapper>
       <Perks />
     </>
