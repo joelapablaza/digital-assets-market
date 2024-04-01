2d2dcda4871f7e4c753cd0c019b52eef00cf7c6a
joelapablaza <joelapablaza@gmail.com>
2024-04-01 13:27:14 -0300

    feat: middleware to get user information from the server

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import { Metadata } from 'next';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -27,4 +28,47 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice);
+}
+
+export function constructMetadata({
+  title = 'Capi Market - el mercado para assets digitales de Argentina',
+  description = 'Capi Market tiene la idea de fomentar la competencia y crecimiento del mercado digial local.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joelapablaza',
+    },
+    icons,
+    metadataBase: new URL('https://demo.com.ar'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
